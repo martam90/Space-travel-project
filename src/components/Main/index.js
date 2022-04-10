8345ee0e929dc33607601229183c5255de772b02
Marta <65865826+martam90@users.noreply.github.com>
2022-04-10 17:33:16 +0200

    Fixes (#6)
    * add tech page + fix styles
    
    * add 404 + fix styles

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,18 +6,16 @@ const Main = () => {
     <main className="main" id="main">
       <section className="main__homepage--section">
         <div>
-          <h1>
+          <h1 className="main__homepage--heading">
             So, you want to travel to{' '}
             <span className="main__heading-span">Space</span>
           </h1>
-          <div>
-            <p className="main__text">
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
+          <p className="main__text">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
         </div>
         <Link to="/destination" className="cta">
           Explore
---
+++ src/components/MainTech/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import pagesData from '../../../assets/pagesData';
+import { useMediaQuery } from 'react-responsive';
+
+const MainTech = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+  let currentTech = pagesData.technology[currentTab];
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1216px)',
+  });
+
+  const isMobileorTablet = useMediaQuery({
+    query: '(max-width: 1215px)',
+  });
+
+  return (
+    <main className="main__tech" id="main">
+      <div className="main__title">
+        <span className="main__title--number">03</span>
+        <span className="main__title--name">Space Launch 101</span>
+      </div>
+      <div className="main__content--bottom-wrapper">
+        <div className="main__tech-img-wrapper">
+          {isMobileorTablet && (
+            <img
+              className="main__tech-img"
+              src={currentTech.images.landscape}
+              alt={currentTech.name}
+            />
+          )}
+
+          {isDesktopOrLaptop && (
+            <img
+              className="main__tech-img"
+              src={currentTech.images.portrait}
+              alt={currentTech.name}
+            />
+          )}
+        </div>
+        <div className="main__nav--tech" aria-label="tab navigation">
+          {pagesData.technology.map((tech, index) => (
+            <button
+              role="tab"
+              key={index}
+              aria-label="tab button"
+              aria-selected={currentTab === index ? 'true' : 'false'}
+              className={
+                currentTab === index
+                  ? 'main__nav--bullet active'
+                  : 'main__nav--bullet'
+              }
+              onClick={() => setCurrentTab(index)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+
+        <div className="main__content--bottom-tech">
+          <p className="main__content--bottom-subtitle">
+            The terminology&nbsp;...
+          </p>
+          <h1 className="main__content--bottom-title">{currentTech.name}</h1>
+          <p className="main__content--bottom-desc">
+            {currentTech.description}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainTech;
