4f0da2983e2de9e36ca82077cfe14ce17c10913a
martam90 <banas.m.marta@gmail.com>
2022-04-12 17:35:08 +0200

    add plugin link canonical

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.space-travel.vercel.app',
     title: 'Space travel',
     description:
       'Space tourism is a project that takes you beyond your dreams and expectations. Book a tour with our agency and discover the Universe!',
     author: 'martam90',
     keywords: 'planets, journey, travel, space, adventure',
-    image: 'static/images/image-moon.png',
+    image: 'static/images/crew/image-douglas-hurley.png',
   },
   plugins: [
     'gatsby-plugin-sass',
@@ -18,6 +17,12 @@ module.exports = {
       resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'static/images/favicon-32x32.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://space-travel-project.vercel.app/',
       },
     },
   ],
